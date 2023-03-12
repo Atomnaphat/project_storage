645eb787e4a81ad6b7eab28cc81d2c354bccb91d
napat thammachot <napat1970@gmail.com>
2023-03-12 10:18:42 +0700

    เพิ่มได้สำเร็จแต่ลบไม่ได้

--- page/admin.php
+++ page/admin.php
@@ -1,11 +1,17 @@
 <?php
 if(isset($_POST['save'])){
-    $id = "admin-".date("U");
-    $name = $POST['name'];
-    $login = $POST['login'];
+    $id ="admin-".date("U");
+    $name =$_POST['name'];
+    $login =$_POST['login'];
     $pass=$_POST['pass'];
     $sql="insert into admin (admin_id,admin_name,admin_login,admin_password)values('$id','$name','$login','$pass')";
     mysqli_query($conn,$sql);
+}
+if(isset($_POST['del'])){
+    $id = $_GET['del'];
+    $sql = "delete form admin where admin_id = '$id'";
+    mysqli_query($conn,$sql);
+    header('location: ?page=admin');
 }
 ?>
 <!doctype html>
@@ -22,32 +28,15 @@ if(isset($_POST['save'])){
         <b>เพิ่ม</b><hr>
         <form method="post">
             <label>name</label>
-        <input type="text" class ="form-control" name="name">
+        <input type="text" class = "form-control" name = "name">
             <label>login</label>
-        <input type="text" class ="form-control" name="login">
-            <label>pass</label>
-        <input type="text" class ="form-control" name="pass">
+        <input type="text" class = "form-control" name = "login">
+            <label>password</label>
+        <input type="text" class = "form-control" name = "pass">
             <br>
             <input type="submit" class="btn btn-primary" name="save" value="add">
         </form>
         <!--/////-->
-          <!----del-->
-        <?php
-         $id =$_GET['del'];
-         $sql2 = "select * from admin where admin_id = '$id'";
-         $result2 = mysqli_query($conn,$sql2);
-         $row2 = mysqli_fetch_array($result2);
-         //print_r($row2);
-        ?>
-          <b>ลบ</b><hr>
-          <b>คุณต้องการลบ <?php echo $row2['admin_name'];?> หรือไม่</b>
-        <form method="post">
-         <div class="row">
-            <div class="col-sm-6" align ="right"><a href="?page=admin" class="btn btn-secondary">กลับ</a></div>
-            <div class="col-sm-6"></div><input type="submit" class="btn btn-danger" name="del" value="del"></div>
-            </div>
-        </form>
-          <!--/////-->
         </div>
     <div class="col-sm-9">
          <!----view-->
@@ -76,6 +65,25 @@ if(isset($_POST['save'])){
              <?php } ?>
          </table>
          <!--/////-->
+    </div>
+    <div>
+         <!----del-->
+        <?php
+         $id =$_GET['del'];
+         $sql2 = "select * from admin where admin_id = '$id'";
+         $result2 = mysqli_query($conn,$sql2);
+         $row2 = mysqli_fetch_array($result2);
+         //print_r($row2);
+        ?>
+         <b>ลบ</b><hr>
+        <b>คุณต้องการลบ <?php echo $row2['admin_name'];?> หรือไม่</b>
+        <form method="post">
+         <div class="row">
+            <div class="col-sm-6"><a href="?page=admin" class="btn btn-secondary">กลับ</a></div>
+            <div class="col-sm-6"><input type="submit" class="btn btn-danger" name="del" value="del"></div>
+        </div>
+        </form>
+          <!--/////-->
     </div>
 </body>
 </html>
